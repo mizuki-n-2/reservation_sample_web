19f1321dc2aca038f7003ee5484c2cd3b11a0d1c
mizuki-n-2 <mizukizhangze@gmail.com>
2022-01-28 17:48:12 +0900

    feat: reservation confirm

---
+++ components/ReservationConfirmCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="mt-2">
+    <v-card>
+      <v-card-title class="d-block text-center">
+        予約情報
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>予約ID： {{ reservation.id }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>予約日： {{ reservation.date }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>予約時間： {{ reservation.hour }}開始</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>予約人数： {{ reservation.number }}人</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>予約者： {{ reservation.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>メールアドレス： {{ reservation.email }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>電話番号： {{ reservation.phone }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reservation: {
+      type: Object,
+      required: true,
+      default: () => ({
+        id: 0,
+        date: '',
+        hour: '',
+        number: 0,
+        name: '',
+        email: '',
+        phone: '',
+      }),
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,60 @@
 <template>
   <v-row>
     <v-col>
+      <h2 class="my-3">予約状況</h2>
       <reservation-status @sendReservation="text = $event"></reservation-status>
       <p>{{ text }}</p>
+    </v-col>
+    <v-col>
+      <h2 class="my-3">予約確認</h2>
+      <v-text-field
+        v-model="searchReservationId"
+        label="確認したい予約のIDを入力してください"
+        append-outer-icon="mdi-magnify"
+        @click:append-outer="searchReservation"
+        @keydown.enter="searchReservation"
+      ></v-text-field>
+      <div v-if="matchedReservation">
+        <reservation-confirm-card :reservation="matchedReservation"></reservation-confirm-card>
+      </div>
+      <p v-if="!beforeSearch && !matchedReservation">
+        一致する予約はありません
+      </p>
     </v-col>
   </v-row>
 </template>
 
 <script>
 import ReservationStatus from '~/components/ReservationStatus.vue'
+import ReservationConfirmCard from '~/components/ReservationConfirmCard.vue'
 
 export default {
   components: {
-    ReservationStatus
+    ReservationStatus,
+    ReservationConfirmCard,
   },
   data() {
     return {
-      text: '日程未選択です'
+      beforeSearch: true,
+      searchReservationId: '',
+      text: '日程未選択です',
+      reservations: [{
+        id: "reservation-id-1",
+        date: '2022年6月11日(土)',
+        hour: '10:00',
+        number: 2,
+        name: 'マイケル・ジョンソン',
+        email: 'aaa@sample.com',
+        phone: '000-0000-0000',
+      }],
+      matchedReservation: null,
     }
+  },
+  methods: {
+    searchReservation() {
+      this.beforeSearch = false
+      this.matchedReservation = this.reservations.find(reservation => reservation.id === this.searchReservationId)
+    },
   },
 }
 </script>
